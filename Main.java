eea4fce04c69739ae3d4946e27f67fc9372a0f7d
Sitzmaa <alex@sitzman.net>
2023-05-23 18:28:12 -0700

    added UI

--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
+import java.util.Scanner;
+
 // Alex Sitzman
 public class Main {
     public static void main(String[] args) {
-        if(args.length<2) {
-            System.err.println("usage: total <coin values>");
-            return;
-        }
-        
-        int total = Integer.parseInt(args[0]); // find the total value to add up to
+
+        Scanner sc = new Scanner(System.in);
+        int total = promptTotal(sc); // find the total value to add up to
         int largest = 0;
-        for (int i = 1; i < args.length; i++) { // find the largest coin value
-            int holder = Integer.parseInt(args[i]);
+        String[] coins = promptUser(sc);
+        sc.close();
+        for (int i = 1; i < coins.length; i++) { // find the largest coin value
+            int holder = Integer.parseInt(coins[i]);
             if (largest < holder) {
                 largest = holder;
             }
@@ -22,14 +23,27 @@ public class Main {
         for (int i = 0; i < largest; i++) {
             coinValues[i] = 0;
         }
-        for (int i = 1; i < args.length; i++) {
-            int value = Integer.parseInt(args[i]);
+        for (int i = 1; i < coins.length; i++) {
+            int value = Integer.parseInt(coins[i]);
             coinValues[value] = value;
         }
 
         // Call solver function
         CoinParser cParser = new CoinParser(coinValues);
         cParser.initialize(total);
+    }
+
+    public static String[] promptUser(Scanner sc) {
+        System.out.print("Enter a coin values: ");
+        String input = sc.nextLine();
+        String[] args = input.split(" ");
+        return args;
+    }
+
+    public static int promptTotal(Scanner sc) {
+        System.out.print("Enter a value: ");
+        int input = Integer.parseInt(sc.nextLine());
+        return input;
     }
 }
 
